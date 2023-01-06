d6639e28c7f0afee63b34f581c2ba28ff4a6f51a
Felipe Teixeira <felipeteixeira2023@gmail.com>
2023-01-06 17:08:15 -0300

    Exercicio Basico

---
+++ 1000/Program.cs
@@ -0,0 +1,15 @@
+﻿// Exercicio 1000 beecrowd
+// Só um programa que imprime Hello World na tela
+
+using System; 
+
+namespace _1000
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
