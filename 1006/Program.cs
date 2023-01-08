b27d1c5b7c11a7d70be9918a7c9baca53f352819
Felipe Teixeira <felipeteixeira2023@gmail.com>
2023-01-08 17:03:58 -0300

    Média ponderada dnv

---
+++ 1006/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Valor1;
+            double Valor2;
+            double Valor3;
+            double Media;
+
+            Valor1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Valor2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Valor3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Media = ((Valor1 * 2.0) + (Valor2 * 3.0) + (Valor3 * 5.0)) / 10.0;
+            Console.WriteLine("MEDIA = " + Media.ToString("F1", CultureInfo.InvariantCulture));
+
+        }
+
+    }
+}
