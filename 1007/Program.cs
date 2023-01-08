65a15042c64a00ff801cc1e0f4ee589f6d203144
Felipe Teixeira <felipeteixeira2023@gmail.com>
2023-01-08 17:12:25 -0300

    Produto e diferença

---
+++ 1007/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Valor1, Valor2, Valor3, Valor4;
+            int Diferenca;
+            Valor1 = int.Parse(Console.ReadLine());
+            Valor2 = int.Parse(Console.ReadLine());
+            Valor3 = int.Parse(Console.ReadLine());
+            Valor4 = int.Parse(Console.ReadLine());
+            Diferenca = (Valor1*Valor2)-(Valor3*Valor4);
+            Console.WriteLine("DIFERENCA = " + Diferenca);
+        }
+    }
+}
