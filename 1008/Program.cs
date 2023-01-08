3216c8f877991189392d206cefd899f0d65cb2b3
Felipe Teixeira <felipeteixeira2023@gmail.com>
2023-01-08 17:27:36 -0300

    Salario por hora

---
+++ 1008/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumeroFuncionario;
+            int HorasTrabalhadas;
+            double ValorHora;
+            double Salario;
+
+            NumeroFuncionario = int.Parse(Console.ReadLine());
+            HorasTrabalhadas = int.Parse(Console.ReadLine());
+            ValorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Salario = (ValorHora * HorasTrabalhadas);
+            Console.WriteLine("NUMBER = "+NumeroFuncionario);
+            Console.WriteLine("SALARY = U$ "+Salario.ToString("F2",CultureInfo.InvariantCulture));
+
+        }
+    }
+}
