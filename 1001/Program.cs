7b29886eef2212ff96edac4d439c404011d52a0f
Felipe Teixeira <felipeteixeira2023@gmail.com>
2023-01-08 16:08:42 -0300

    Soma básica

---
+++ 1001/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace _1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Valor1;
+            int Valor2;
+            Valor1 = int.Parse(Console.ReadLine());
+            Valor2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("X = "+ (Valor1+Valor2));
+        }
+    }
+}
