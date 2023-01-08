3e31c50c4b765467528578930c10377961629cc2
Felipe Teixeira <felipeteixeira2023@gmail.com>
2023-01-08 16:38:44 -0300

    Produto simples

---
+++ 1004/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace _1004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Valor1;
+            int Valor2;
+            Valor1 = int.Parse(Console.ReadLine());
+            Valor2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("PROD = " + (Valor1 * Valor2));
+        }
+    }
+}
