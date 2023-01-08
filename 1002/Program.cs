8ca70098e897d6c9b2ee6be4c1f8bf8de40774f2
Felipe Teixeira <felipeteixeira2023@gmail.com>
2023-01-08 16:26:53 -0300

    Área do círculo

---
+++ 1002/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Pi;
+            double Raio;
+            double Area;
+
+            Pi = 3.14159;
+            Raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Area = (Raio * Raio * Pi);
+
+            Console.WriteLine("A="+Area.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
