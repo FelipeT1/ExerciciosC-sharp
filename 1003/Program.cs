b94dfe20079aa4080e6529e5d981ed97bf490e22
Felipe Teixeira <felipeteixeira2023@gmail.com>
2023-01-08 16:34:25 -0300

    Soma Simples

---
+++ 1003/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace _1003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Valor1;
+            int Valor2;
+            Valor1 = int.Parse(Console.ReadLine());
+            Valor2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("SOMA = " + (Valor1 + Valor2));
+        }
+    }
+}
