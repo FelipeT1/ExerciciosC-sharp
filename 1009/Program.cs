8a76e6b6d079bed4fd40320751093266e9e2c8c3
Felipe Teixeira <felipeteixeira2023@gmail.com>
2023-01-10 20:35:47 -0300

    Montante

---
+++ 1009/Program.cs
@@ -0,0 +1,24 @@
+﻿using System; 
+using System.Globalization;
+
+namespace _1009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nome;
+            double Salario;
+            double TotalVenda;
+            double Montante;
+
+            Nome = Console.ReadLine();
+            Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            TotalVenda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Montante = 15 * TotalVenda / 100 + Salario;
+
+            Console.WriteLine("TOTAL = R$ " + Montante.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
