e0bf005fee0721ef64705c594e0571e2c59bafd7
Felipe Teixeira <felipeteixeira2023@gmail.com>
2023-01-11 03:02:07 -0300

    Volume esfera

---
+++ 1011/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace _1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Pi = 3.14159;
+            double Raio;
+            double Volume;
+            Raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Volume = (Pi * Raio * Raio * Raio) * (4.0 / 3.0);
+            Console.WriteLine("VOLUME = " + Volume.ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
