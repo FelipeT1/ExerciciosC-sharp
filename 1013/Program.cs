5f06ce7ef5b7876e971dda66f1c149974710a12a
Felipe Teixeira <felipeteixeira2023@gmail.com>
2023-01-11 03:30:38 -0300

    Maior entre 3 numeros inteiros sem condicionais

---
+++ 1013/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+
+namespace _1013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Valores;
+            Valores = Console.ReadLine();
+            string[] V = Valores.Split(' ');
+            int A = int.Parse(V[0]);
+            int B = int.Parse(V[1]);
+            int C = int.Parse(V[2]);
+            int MaiorAB = (A + B + Math.Abs(A - B)) / 2;
+            int MaiorABC = (MaiorAB + C + Math.Abs(MaiorAB - C)) / 2;
+
+            Console.WriteLine(MaiorABC+" eh o maior");
+
+
+
+
+
+        }
+    }
+}
