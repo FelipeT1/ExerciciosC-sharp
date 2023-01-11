5257a17e6ff5f371cef5695a825dc2189191244b
Felipe Teixeira <felipeteixeira2023@gmail.com>
2023-01-11 04:07:11 -0300

    Distancia entre dois pontos

---
+++ 1015/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace _1015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores1 = Console.ReadLine().Split(' ');
+            double x1 = double.Parse(valores1[0], CultureInfo.InvariantCulture);
+            double y1 = double.Parse(valores1[1], CultureInfo.InvariantCulture);
+            string[] valores2 = Console.ReadLine().Split(' ');
+            double x2 = double.Parse(valores2[0], CultureInfo.InvariantCulture);
+            double y2 = double.Parse(valores2[1], CultureInfo.InvariantCulture);
+            double Distancia = ((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            Distancia = Math.Pow(Distancia,1.0/2.0);
+            Console.WriteLine(Distancia.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
