cef5bb81fa5baf2af3a13fa30680412184cdf60a
Felipe Teixeira <felipeteixeira2023@gmail.com>
2023-01-11 03:39:11 -0300

    Consumo Km/L

---
+++ 1014/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace _1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int DistanciaPercorrida;
+            double CombustivelGasto;
+            double Consumo;
+            DistanciaPercorrida = int.Parse(Console.ReadLine());
+            CombustivelGasto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Consumo = DistanciaPercorrida / CombustivelGasto;
+            Console.WriteLine(Consumo.ToString("F3", CultureInfo.InvariantCulture)+" km/l");
+
+
+
+
+
+        }
+    }
+}
