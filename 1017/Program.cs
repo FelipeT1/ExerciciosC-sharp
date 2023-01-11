fecf036f085d0f16d6d0b312d55f2862813da4c6
Felipe Teixeira <felipeteixeira2023@gmail.com>
2023-01-11 04:42:35 -0300

    Combustivel gasto

---
+++ 1017/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Velocidade;
+            int Tempo;
+            double Litros;
+            Velocidade = int.Parse(Console.ReadLine());
+            Tempo = int.Parse(Console.ReadLine());
+            Litros = ((double)(Velocidade * Tempo)/12);
+
+            Console.WriteLine(Litros.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
