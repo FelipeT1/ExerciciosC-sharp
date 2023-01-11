4fe61461b8fa076ac6b5b581f655e6893e90a0f5
Felipe Teixeira <felipeteixeira2023@gmail.com>
2023-01-11 04:29:03 -0300

    Distancia e Velocidade = Tempo

---
+++ 1016/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace _1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Distancia;
+            int Tempo;
+            Distancia = int.Parse(Console.ReadLine());
+            Tempo = Distancia * 2;
+            Console.WriteLine(Tempo+" minutos");
+        }
+    }
+}
