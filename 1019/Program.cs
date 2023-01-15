2c96a4674565b48af663c8f827b0a4642e60c169
Felipe Teixeira <felipeteixeira2023@gmail.com>
2023-01-15 10:31:45 -0300

    Divisao e resto para hora, minuto e segundo

---
+++ 1019/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace _1019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ValorSegundo = int.Parse(Console.ReadLine());
+            
+            int ValorMin = ValorSegundo / 60;
+            int ValorHora = ValorMin / 60;
+            ValorSegundo = ValorSegundo - ValorMin * 60;
+            ValorMin = ValorMin % 60;
+            
+            
+            
+            Console.WriteLine(ValorHora+":"+ValorMin+":"+ValorSegundo);
+
+
+
+        }
+    }
+}
