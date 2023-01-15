96c48119a756268bb1c1ab8ec49e435ee19ba77d
Felipe Teixeira <felipeteixeira2023@gmail.com>
2023-01-15 11:03:06 -0300

    Divisao e resto para ano, mes e dia

---
+++ 1020/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+
+namespace _1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Idade = int.Parse(Console.ReadLine());
+            int Ano = Idade / 365;
+            int Mes = Idade % 365;
+            Mes = Mes / 30;
+            int Dia = (Idade % 365) % 30;
+            Console.WriteLine((Ano)+" ano(s)");
+            Console.WriteLine((Mes)+" mes(es)");
+            Console.WriteLine((Dia)+" dia(s)");
+
+        }
+    }
+}
